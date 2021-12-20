85ee57540de984d8f0126e2f3a8fc2a651f5cd8a
Jerome Wy <jeromedaniel.wy@gmail.com>
2021-12-20 14:03:01 -0800

    VS Code Prettier working again. Working on posts/comments forms

--- server/db/index.js
+++ server/db/index.js
@@ -1,18 +1,21 @@
-const mongoose = require('mongoose')
-const env = require('dotenv').config();
+const mongoose = require("mongoose");
+const env = require("dotenv").config();
 
 mongoose
-  .connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wuv3l.mongodb.net/NerdFeedDatabase?retryWrites=true&w=majority`,
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true})
-  .then(() => {
-    console.log('Successfully connected to MongoDB.')
-  })
-  .catch((e) => {
-    console.error('Connection error', e.message)
-  })
-mongoose.set('debug', true)
-const db = mongoose.connection
+	.connect(
+		`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wuv3l.mongodb.net/NerdFeedDatabase?retryWrites=true&w=majority`,
+		{
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		}
+	)
+	.then(() => {
+		console.log("Successfully connected to MongoDB.");
+	})
+	.catch((e) => {
+		console.error("Connection error", e.message);
+	});
+mongoose.set("debug", true);
+const db = mongoose.connection;
 
-module.exports = db
+module.exports = db;
